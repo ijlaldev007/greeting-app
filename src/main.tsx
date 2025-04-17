107885ab13e50a12dc4074b6b4cc823adcbb583b
ijlaldev007 <muhammad13ijlal@gmail.com>
2025-04-17 14:12:36 +0500

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './index.css'; // or './App.css' — whichever you added Tailwind to
-import App from './App.tsx'
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <h1 className="text-3xl font-bold text-purple-500">Tailwind is working!</h1>
-
   </StrictMode>,
-)
+);
