ee67482e61f78b03a70c4d59cec2c630e5262526
Sohaib <shobiikhanzada@gmail.com>
2025-04-17 18:23:17 +0500

    feature reuseable vedio component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import backgroundImage from "./assets/images/background-pic.jpg";
 import SplashScreen from "./pages/splash/SplashScreen";
+import Video from "./components/vedio/index";
 
 function App() {
   return (
@@ -8,7 +9,11 @@ function App() {
       className="relative min-h-screen w-full bg-cover bg-center bg-no-repeat flex items-center justify-center overflow-hidden p-4"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <SplashScreen />
+      {/* <SplashScreen /> */}
+      <Video
+        videoUrl="https://youtube.com/shorts/1Zcvyft5-dQ?si=ryW0HGFSrGMnq__R" // Example YouTube Shorts URL
+        title="Sample YouTube Shorts Video"
+      />
     </div>
   );
 }
