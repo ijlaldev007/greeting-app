21496fa649683fb3938e023d48237010cc02e1a4
ijlaldev007 <muhammad13ijlal@gmail.com>
2025-04-19 00:13:23 +0500

    Uninstalled swiper

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,3 @@
 /// <reference types="vite/client" />
 
 declare module '*.css';
-declare module 'swiper/*';
-declare module 'swiper/css';
-declare module 'swiper/css/navigation';
-declare module 'swiper/css/pagination';
