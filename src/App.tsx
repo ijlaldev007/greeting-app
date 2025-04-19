8b31f055c34027b472f45bced894a120fd6ae70b
Sohaib <shobiikhanzada@gmail.com>
2025-04-20 00:14:02 +0500

    feature sign up page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import backgroundImage from "./assets/images/background-pic.jpg";
-import OnboardingScreen from "./pages/onboarding/OnBoardingVideoScreen";
+import SignUp from "./pages/signup/SignUp";
 function App() {
   return (
     <div
-      className="relative min-h-screen w-full bg-cover bg-center bg-no-repeat flex items-center justify-center overflow-hidden p-4"
+      className="relative min-h-screen w-full bg-cover bg-center bg-no-repeat flex items-center justify-center overflow-hidden "
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <OnboardingScreen />
+      <SignUp />
     </div>
   );
 }
