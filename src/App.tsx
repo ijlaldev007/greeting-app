c9420ac00023ebb4c0574c60e2bb8f3b1df0b272
Sohaib <shobiikhanzada@gmail.com>
2025-04-21 14:57:59 +0500

    bugfix for no conflic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import "./App.css";
 import backgroundImage from "./assets/images/background-pic.jpg";
-import GreetingDetailsPage from "./pages/greetingdetails/GreetingDetails";
 
 function App() {
   return (
     <div
       className="relative min-h-screen w-full bg-cover bg-center bg-no-repeat flex items-center justify-center overflow-hidden "
       style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <GreetingDetailsPage />
-    </div>
+    ></div>
   );
 }
 
