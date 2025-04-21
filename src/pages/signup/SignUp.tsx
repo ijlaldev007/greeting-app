560ef0bd1c8144f4c0330843cc8fda47008c11e3
ijlaldev007 <muhammad13ijlal@gmail.com>
2025-04-21 21:09:25 +0500

    Remove unnecessary padding from SignUp page

--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -14,20 +14,20 @@ const SignUp = () => {
   };
 
   return (
-    <div className='min-h-screen flex flex-col items-center justify-between px-4 sm:px-6 md:px-10 py-10'>
+    <div className='min-h-screen flex flex-col items-center justify-between px-4 sm:px-6 md:px-10 pt-6 pb-0'>
       {/* Top Heading */}
-      <h1 className='mb-8 text-center text-gray-800 text-[25px] font-semibold font-montserrat leading-[100%]'>
+      <h1 className='mb-4 text-center text-gray-800 text-[25px] font-semibold font-montserrat leading-[100%]'>
         Sign Up
       </h1>
 
       {/* Inputs */}
-      <div className='w-full max-w-sm space-y-4'>
+      <div className='w-full max-w-sm space-y-3'>
         <NumberInput />
         <EmailInput type='email' placeholder='Email' label='Email' />
       </div>
 
       {/* Masha Image */}
-      <div className='mt-10 sm:mt-12 md:mt-14'>
+      <div className='mt-6'>
         <img
           src={mashaImage}
           alt='Masha'
@@ -36,7 +36,7 @@ const SignUp = () => {
       </div>
 
       {/* Below Image Heading */}
-      <h2 className='mt-6 text-center text-gray-800 text-[20px] sm:text-[22px] md:text-[25px] font-semibold font-montserrat leading-[100%] px-2 sm:px-6'>
+      <h2 className='mt-4 text-center text-gray-800 text-[20px] sm:text-[22px] md:text-[25px] font-semibold font-montserrat leading-[100%] px-2 sm:px-6'>
         The video greeting will be sent to this contact
       </h2>
 
